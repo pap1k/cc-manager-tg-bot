0a646827c201beaa7ddacb885e2ba8582efad300
deniska <dmitriev.da@trytek.ru>
2025-03-18 14:57:23 +0300

    first

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import URL, create_engine, text
+import asyncio
+from config import settings
+
+engine = create_async_engine(
+    url=settings.DATABASE_URL,
+    echo=False,
+    # pool_size=5
+)
+
+async def test():
+    async with engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3"))
+        print(f"{res.all()=}")
+
+asyncio.run(test())
